152acdb970338dbb179b25837bfd4778b9cf02cb
Ayush <ayushnvijay@gmail.com>
2015-02-22 22:56:01 -0500

    test v1.2

--- test/secret_test.js
+++ test/secret_test.js
@@ -5,18 +5,19 @@ var pem = fs.readFileSync('../../oada-id-client-js/examples/server-client/privke
 //found these keys in Alex's repo
 var pem2 = fs.readFileSync('../../oada-id-client-js/examples/server-client/pubkey.pem');
 var kid = '1234';
-var key = {pem:pem, kid:kid};
-var key2 = {pem:pem2,kid:kid};
+var key = {kty:'PEM',pem:pem, kid:kid};
+var key2 = {kty:'PEM',pem:pem2,kid:kid};
 describe("testing if generate(key) returns undefined even after passing PEM ",function(){
-    it("should not pass as key is PEM (using deep equal)",function(done){
-        expect(secret.generate(key)).to.deep.equal(undefined);
+    it("should pass as key is PEM (using deep equal)",function(done){
+        expect(secret.generate(key)).not.to.deep.equal(undefined);
         done();
     });
 });
-//though keys are pem, generate(key) is returning value "undefined" and tests are passing!
 describe("testing if generate(key) returns undefined even after passing PEM",function(){
-    it("should not pass as key is PEM (using equal)",function(done){
-        expect(secret.generate(key2)).to.equal(undefined);
+    it("should pass as key is PEM (using equal)",function(done){
+        //shall I handle an error that public key has been passed
+        expect(secret.generate(key2)).not.to.deep.equal(undefined);
+        //gives signFinal error when key is public.
         done();
     });
 });
